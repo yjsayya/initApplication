023d9767727dabf3a00f0115445168026df12aa8
sayya <djdj9876@naver.com>
2025-03-24 10:35:15 +0900

    fix: HIP 메모리 변수 수정

--- src/main/resources/run.sh
+++ src/main/resources/run.sh
@@ -7,26 +7,28 @@ JAVA_HOME=/app/java/jdk1.8.0
 APP_HOME=`pwd` # 현재 dir
 APP_NAME="jar파일이름" # BootJar 후에 생긴 .jar 파일
 APPLICATION=$APP_HOME/$APP_NAME
-$SERVICE_NAME=서비스이름
+SERVICE_NAME=서비스이름
 
 #CONF=$APP_HOME/application.yml
-
-# 3. Java Options
-MIN_HIP_MEMORY=1024m
-MAX_HIP_MEMORY=1024m
-
-JAVA_OPTS="-server"
-JAVA_OPTS="$JAVA_OPTS -Dservice.name=$SERVER_NAME"
-JAVA_OPTS="$JAVA_OPTS -Xms$MIN_HIP_MEMORY -Xmx$MAX_HIP_MEMORY"
-JAVA_OPTS="$JAVA_OPTS -Dspring.profiles.active=prod"
-#JAVA_OPTS="$JAVA_OPTS -Dspring.profiles.active=prod"
 
 # 4.PID
 PID_FILE=$SERVICE_NAME.pid
 
+# 3. Java Options
+MIN_HEAP_MEMORY=1024m
+MAX_HEAP_MEMORY=1024m
+
+JAVA_OPTS="-server"
+JAVA_OPTS="$JAVA_OPTS -Dservice.name=$SERVICE_NAME"
+JAVA_OPTS="$JAVA_OPTS -Xms$MIN_HEAP_MEMORY -Xmx$MAX_HEAP_MEMORY"
+JAVA_OPTS="$JAVA_OPTS -Dspring.profiles.active=dev"
+#JAVA_OPTS="$JAVA_OPTS -Dspring.profiles.active=prod"
+
+JAVA_CMD="$JAVA_HOME/bin/java $JAVA_OPTS -jar $APPLICATION"
+
 start() {
   if [ -f "$PID_FILE" ] && kill -0 $(cat "$PID_FILE") 2>/dev/null; then
-    echo "$SERVICE_NAME is already running with PID $(cat $PID_FILE)."
+    echo "$SERVICE_NAME Is Already Running with PID $(cat $PID_FILE)."
     exit 1
   fi
   echo "Starting $SERVICE_NAME..."
